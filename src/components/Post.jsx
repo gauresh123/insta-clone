cff82808d6eb404f1bbfe8a2fa2c2aa41e88576e
gauresh123 <waranggauresh@gmail.com>
2025-01-12 22:50:16 +0530

    changes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -19,7 +19,7 @@ function Post({ handleOpen, data, setData, setCurrentUser }) {
 
   const handleLikeWithAnimation = (item) => {
     setAnimate(true);
-    setTimeout(() => setAnimate(false), 500);
+    setTimeout(() => setAnimate(false), 400);
     handleLike(item);
   };
 
